56006dec1e4fd1ecc545a66987819a81dc03c7c5
nazar-mazuryk1 <nmazuryk-ecw@cricut.com>
2024-10-09 15:22:20 +0300

    Resolve connector url from env variables

--- kafka-to-elasticsearch-read-configs-from-enr-variable/startup.sh
+++ kafka-to-elasticsearch-read-configs-from-enr-variable/startup.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-/etc/confluent/docker/run &
-KAFKA_CONNECT_PID=$!
+# Start Kafka Connect in the background
+/etc/confluent/docker/run & 
+KAFKA_CONNECT_PID=$!  # Capture the process ID of the Kafka Connect task
 
-echo "Kafka Connect is running with PID: $KAFKA_CONNECT_PID"
+# Use environment variables to construct the Kafka Connect URL
+CONNECT_URL="http://${CONNECT_REST_ADVERTISED_HOST_NAME}:${CONNECT_REST_PORT}"
 
-echo "Waiting for Kafka Connect to start listening on kafka-connect ⏳"
-while [ $(curl -s -o /dev/null -w %{http_code} http://localhost:8083/connectors) -eq 000 ]; do 
-  echo -e $(date) " Kafka Connect listener HTTP state: " $(curl -s -o /dev/null -w %{http_code} http://localhost:8083/connectors) " (waiting for 200)"
+echo "Waiting for Kafka Connect to start listening on $CONNECT_URL ⏳"
+while [ $(curl -s -o /dev/null -w %{http_code} $CONNECT_URL/connectors) -eq 000 ]; do 
+  echo -e $(date) " Kafka Connect listener HTTP state: " $(curl -s -o /dev/null -w %{http_code} $CONNECT_URL/connectors) " (waiting for 200)"
   sleep 5 
 done;
 
-echo 'Kafka Connect is up! Registering Elasticsearch sink connectors...'
-echo "Registering connector with configuration: $ELASTICSEARCH_SINK_CONFIGS"
+echo "Kafka Connect is up! Registering Elasticsearch sink connectors..."
 
+echo "Registering connector with configuration: $ELASTICSEARCH_SINK_CONFIGS"
 echo "$ELASTICSEARCH_SINK_CONFIGS" | jq -c '.[]' | while read -r SINK; do
   echo "Registering $SINK"
-  curl -X POST -H "Content-Type: application/json" --data "$SINK" http://localhost:8083/connectors
+  curl -X POST -H "Content-Type: application/json" --data "$SINK" $CONNECT_URL/connectors
 done
 
 # Now bring Kafka Connect back to the foreground
